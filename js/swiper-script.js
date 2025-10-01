3fded5f3d0d49602c5484c360033d6f0a4f35f72
benhens <peter@metera.co>
2025-10-01 19:43:44 +0300

    Edits to the page (property looks etc)

--- js/swiper-script.js
+++ js/swiper-script.js
@@ -1,41 +1,3 @@
-$(function () {
-    var swiperPartner = new Swiper('.swiper.swiperPartner', {
-        autoplay: {
-            delay: 3000
-        },
-        speed: 2000,
-        slidesPerView: 5,
-        slidesPerGroup: 1,
-        spaceBetween: 10,
-        loop: false,
-        grabCursor: true,
-        breakpoints: {
-            // when window width is >= 360px
-            360: {
-                slidesPerView: 2,
-                slidesPerGroup: 1,
-            },
-            // when window width is >= 768px
-            768: {
-                slidesPerView: 2,
-                slidesPerGroup: 1,
-            },
-            // when window width is >= 1024px
-            1024: {
-                slidesPerView: 5,
-                slidesPerGroup: 1,
-            }
-        },
-        // If we need pagination
-        pagination: {
-            enabled: false,
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true,
-        },
-    });
-
-});
 
 $(function () {
     var swiperCard = new Swiper('.swiper.swiperCard', {
@@ -302,5 +264,3 @@ $(function () {
     });
 
 });
-
-
